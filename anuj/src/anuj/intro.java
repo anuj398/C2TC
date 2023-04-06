08f929550774c93b33acd130a7a377b9168a27a0
Anuj kumar shukla <Anuj kumar shukla@DESKTOP-HLQ8ART>
2023-04-06 09:14:15 +0530

    commit

---
+++ anuj/src/anuj/intro.java
@@ -0,0 +1,10 @@
+package anuj;
+
+public class intro {
+
+	public static void main(String[] args) {
+		System.out.println("hii, good morning");
+
+	}
+
+}
---
+++ anuj/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Anuj kumar shukla
+ *
+ */
+module anuj {
+}
