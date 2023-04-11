79d3bccfde2b514771135ae69860f28636ce4d21
Anuj kumar shukla <Anuj kumar shukla@DESKTOP-HLQ8ART>
2023-04-11 12:01:04 +0530

    3Commit

---
+++ anuj/src/Accessmodifier/Student1.java
@@ -0,0 +1,19 @@
+package Accessmodifier;
+
+public class Student1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Students s1 = new Students(111,"Anujkumar");  
+	    Students s2 = new Students(222,"Krishnmohan");  
+	    Students s3 = new Students(333,"Chellakutty"); 
+	    Students s4 = new Students(444,"Cuitepie");
+	    //calling method to display the values of object  
+	    s1.display();  
+	    s2.display(); 
+	    s3.display();
+	    s4.display();
+	
+	}
+
+}
---
+++ anuj/src/Accessmodifier/Students.java
@@ -0,0 +1,16 @@
+package Accessmodifier;
+
+public class Students {
+	  int id;  
+	    String name;  
+	    //creating a parameterized constructor  
+	    Students (int i,String n){  
+	    id = i;  
+	    name = n;  
+	    }  
+	    //method to display the values  
+	    void display()
+	    {
+	    	System.out.println(id+" "+name);
+	    	}  
+}
