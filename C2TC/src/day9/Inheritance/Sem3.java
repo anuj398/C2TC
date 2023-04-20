2fc40ef6a1466e6bfa88e01558c9851bcf0b97f4
Anuj kumar shukla <Anuj kumar shukla@DESKTOP-HLQ8ART>
2023-04-20 23:20:57 +0530

    commit

---
+++ C2TC/src/day9/Inheritance/Sem3.java
@@ -0,0 +1,15 @@
+package day9.Inheritance;
+
+public class Sem3 extends Sem2 {
+	int m31,m32,m33,m34,avg3;
+	 Sem3()
+	 {
+	   m31=87;
+	   m32=78;
+ 	   m33=88;
+	   m34=96;
+
+       avg3=(m31+m32+m33+m34)/4;
+
+	 }
+}
---
+++ C2TC/src/day9/Inheritance/Sem4.java
@@ -0,0 +1,17 @@
+package day9.Inheritance;
+
+public class Sem4 extends Sem3{
+	int m41,m42,m43,m44,avg4;
+	  Sem4()
+	 {
+	    m41=79;
+	    m42=90;
+	    m43=77;
+	    m44=96;
+	    avg4=(m41+m42+m43+m44)/4;
+	}
+	  int totatavg()
+	{
+	     return (avg1+avg2+avg3+avg4)/4;
+	}
+}
